0f2e50f411a8ea78cd04382adc70d8a691e3c715
Felipe Antero <souzantero@gmail.com>
2023-09-13 12:15:12 -0300

    feat(sale): use react paper to render product item

--- sale/src/components/ProductList.tsx
+++ sale/src/components/ProductList.tsx
@@ -1,4 +1,5 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, View } from "react-native";
+import { Card } from "react-native-paper";
 import { Product } from "@self/domain";
 
 export type ProductListProps = {
@@ -7,11 +8,32 @@ export type ProductListProps = {
 
 export const ProductList = ({ products }: ProductListProps) => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <FlatList
         data={products}
-        renderItem={({ item }) => <Text>{item.name}</Text>}
+        renderItem={({ item, index }) => {
+          return (
+            <View
+              style={{
+                marginBottom: index === 0 ? 0 : 16,
+                marginTop: 16,
+                marginLeft: 16,
+                marginRight: 16,
+              }}
+            >
+              <ProductItem product={item} />
+            </View>
+          );
+        }}
       />
     </View>
+  );
+};
+
+export const ProductItem = ({ product }: { product: Product }) => {
+  return (
+    <Card>
+      <Card.Title title={product.name} subtitle={product.description} />
+    </Card>
   );
 };
