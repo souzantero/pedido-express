28e74e37fe19622cab0193abb08fc1a2fc9344f7
Felipe Antero <souzantero@gmail.com>
2023-09-19 12:28:58 -0300

    feat(order): change product detail add bag button content

--- order/src/components/ProductDetail.tsx
+++ order/src/components/ProductDetail.tsx
@@ -9,7 +9,6 @@ import {
   Divider,
   TextInput,
   IconButton,
-  Banner,
 } from "react-native-paper";
 import { Product } from "@self/domain";
 import { numberToMoney } from "../utilities";
@@ -41,97 +40,83 @@ export const ProductDetail = ({
   };
 
   return (
-    <>
-      <ScrollView
+    <ScrollView
+      style={{
+        flex: 1,
+      }}
+    >
+      <Image
         style={{
-          flex: 1,
+          width: "100%",
+          height: 200,
+        }}
+        source={{ uri: product.displayImageSource }}
+      />
+      <View
+        style={{
+          padding: 16,
         }}
       >
-        <Image
-          style={{
-            width: "100%",
-            height: 200,
-          }}
-          source={{ uri: product.displayImageSource }}
-        />
-        <View
-          style={{
-            padding: 16,
-          }}
-        >
-          <Title
-            style={{
-              fontWeight: "bold",
-            }}
-          >
-            {product.name}
-          </Title>
-          <Text style={{ color: theme.colors.secondary }}>
-            {product.description}
-          </Text>
-          <Paragraph
-            style={{
-              marginTop: 16,
-              color: theme.colors.secondary,
-            }}
-          >
-            {numberToMoney(product.price)}
-          </Paragraph>
-        </View>
-        <Divider />
-        <View
-          style={{
-            padding: 16,
-          }}
-        >
-          <Text style={{ fontWeight: "bold", color: theme.colors.secondary }}>
-            Alguma observação?
-          </Text>
-          <TextInput
-            style={{
-              marginTop: 8,
-            }}
-            mode="outlined"
-            maxLength={140}
-            placeholder="Ex. Tirar a cebola, maionese à parte, etc."
-            value={description}
-            onChangeText={setDescription}
-          />
-        </View>
-        <Divider />
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            margin: 16,
-          }}
-        >
-          <IconButton icon="minus" onPress={decreaseQuantity} />
-          <Text>{quantity}</Text>
-          <IconButton icon="plus" onPress={increaseQuantity} />
-          <Button
-            style={{
-              flex: 1,
-            }}
-            mode="contained"
-            onPress={handleAddProductToOrderBag}
-          >
-            Adicionar ao pedido
-          </Button>
-        </View>
-      </ScrollView>
-      <Banner visible={true}>
-        <Text
+        <Title
           style={{
             fontWeight: "bold",
-            fontSize: 16,
           }}
         >
-          {numberToMoney(totalPrice)}
+          {product.name}
+        </Title>
+        <Text style={{ color: theme.colors.secondary }}>
+          {product.description}
         </Text>
-      </Banner>
-    </>
+        <Paragraph
+          style={{
+            marginTop: 16,
+            color: theme.colors.secondary,
+          }}
+        >
+          {numberToMoney(product.price)}
+        </Paragraph>
+      </View>
+      <Divider />
+      <View
+        style={{
+          padding: 16,
+        }}
+      >
+        <Text style={{ fontWeight: "bold", color: theme.colors.secondary }}>
+          Alguma observação?
+        </Text>
+        <TextInput
+          style={{
+            marginTop: 8,
+          }}
+          mode="outlined"
+          maxLength={140}
+          placeholder="Ex. Tirar a cebola, maionese à parte, etc."
+          value={description}
+          onChangeText={setDescription}
+        />
+      </View>
+      <Divider />
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          margin: 16,
+        }}
+      >
+        <IconButton icon="minus" onPress={decreaseQuantity} />
+        <Text>{quantity}</Text>
+        <IconButton icon="plus" onPress={increaseQuantity} />
+        <Button
+          style={{ flex: 1 }}
+          mode="contained"
+          onPress={handleAddProductToOrderBag}
+        >
+          Adicionar - {numberToMoney(totalPrice)}
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
