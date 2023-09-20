e4039bd68ee65c44a1e93df15dcf56e97d8d293f
Felipe Antero <souzantero@gmail.com>
2023-09-20 16:24:03 -0300

    feat(domain): order status

--- domain/src/entities/order.ts
+++ domain/src/entities/order.ts
@@ -1,10 +1,22 @@
 import { OrderProduct } from "./order-product";
 
+export enum OrderStatus {
+  Pending = 'pending', // This is when the order is created, 
+  Preparing = 'preparing', // This is when the order is being prepared
+  Ready = 'ready', // This is when the order is ready to be picked up
+  Finished = 'finished', // This is when the order is picked up
+}
+
 export interface Order {
   id: string;
   createdAt: Date;
   updatedAt: Date;
   deletedAt?: Date;
+
+  status: OrderStatus;
+
+  isTakeout: boolean;
+  customerName: string;
 
   orderProducts?: OrderProduct[];
 }
