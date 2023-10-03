7c3bda18ba805e5aa3c7eb3c13bb579e0209e2f2
Felipe Antero <souzantero@gmail.com>
2023-10-03 20:37:28 -0300

    refactor(client/app): componentize create order

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -3,24 +3,9 @@ import {
   PaperProvider,
   adaptNavigationTheme,
 } from "react-native-paper";
-import {
-  DefaultTheme as NavigationDefaultTheme,
-  NavigationContainer,
-} from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { OrderBagProvider, ServiceContextProvider } from "./src/context";
-import {
-  CreateOrderScreen,
-  OrderBagScreen,
-  OrderCustomerNameScreen,
-  OrderTakeAwayScreen,
-  ProductCatalogScreen,
-  ProductOrderScreen,
-} from "./src/screens";
-import { CleanOrderBagButton } from "./src/components";
-import { Navigation } from "./src/adapter";
-
-const Stack = createStackNavigator();
+import { DefaultTheme as NavigationDefaultTheme } from "@react-navigation/native";
+import { ServiceContextProvider } from "./src/context";
+import { CreateOrderNavigator } from "./src/components";
 
 export default function App() {
   const { LightTheme } = adaptNavigationTheme({
@@ -40,70 +25,7 @@ export default function App() {
   return (
     <PaperProvider theme={theme}>
       <ServiceContextProvider>
-        <OrderBagProvider>
-          <NavigationContainer theme={theme}>
-            <Stack.Navigator initialRouteName="ProductCatalog">
-              <Stack.Screen
-                name="ProductCatalog"
-                options={{
-                  title: "Bob's",
-                }}
-                component={ProductCatalogScreen}
-              />
-              <Stack.Screen
-                name="ProductOrder"
-                options={{
-                  title: "Pedido do Produto",
-                }}
-                component={ProductOrderScreen}
-              />
-              <Stack.Screen
-                name="OrderBag"
-                options={({ navigation }) => ({
-                  title: "Sacola",
-                  headerRight: () => (
-                    <CleanOrderBagButton
-                      navigation={new Navigation(navigation)}
-                    />
-                  ),
-                })}
-                component={OrderBagScreen}
-              />
-              <Stack.Screen
-                name="OrderTakeAway"
-                options={({ navigation }) => ({
-                  title: "Retirada",
-                  headerRight: () => (
-                    <CleanOrderBagButton
-                      navigation={new Navigation(navigation)}
-                    />
-                  ),
-                })}
-                component={OrderTakeAwayScreen}
-              />
-              <Stack.Screen
-                name="OrderCustomerName"
-                options={({ navigation }) => ({
-                  title: "Nome do Cliente",
-                  headerRight: () => (
-                    <CleanOrderBagButton
-                      navigation={new Navigation(navigation)}
-                    />
-                  ),
-                })}
-                component={OrderCustomerNameScreen}
-              />
-              <Stack.Screen
-                name="CreateOrder"
-                options={{
-                  title: "Pedido",
-                  headerLeft: () => null,
-                }}
-                component={CreateOrderScreen}
-              />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </OrderBagProvider>
+        <CreateOrderNavigator theme={theme} />
       </ServiceContextProvider>
     </PaperProvider>
   );
