830233a679cc51cd16bd0506688a3fd6af296d32
Felipe Antero <souzantero@gmail.com>
2023-10-03 22:16:43 -0300

    feat(client/sdk): find orders

--- client/sdk/src/provider/order-provider.ts
+++ client/sdk/src/provider/order-provider.ts
@@ -1,7 +1,7 @@
-import { CreateOrder, CreateOrderInput, Order, OrderSerializer } from "@pedido-express/core";
+import { CreateOrder, CreateOrderInput, FindOrders, Order, OrderSerializer } from "@pedido-express/core";
 import { Provider } from "./provider";
 
-export class OrderProvider implements CreateOrder {
+export class OrderProvider implements FindOrders, CreateOrder {
   constructor(
     private readonly provider: Provider
   ) { }
@@ -9,6 +9,15 @@ export class OrderProvider implements CreateOrder {
   create(input: CreateOrderInput): Promise<Order> {
     return this.provider.post('/orders', input)
       .then((order: any) => new OrderSerializer(order).serialize());
+  }
+
+  findAllPendingOrders(): Promise<Order[]> {
+    return this.provider.get('/orders/pending')
+      .then((orders: any[]) => orders.map((order) => new OrderSerializer(order).serialize()));
+  }
+  findAllPreparingOrders(): Promise<Order[]> {
+    return this.provider.get('/orders/preparing')
+      .then((orders: any[]) => orders.map((order) => new OrderSerializer(order).serialize()));
   }
 }
 
