4178f8cd3b170e3e79492186a8fb09dccf5f40e1
Felipe Antero <souzantero@gmail.com>
2024-02-20 17:10:18 -0300

    chore: remove firebase functions at time

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,1 @@
-import { onRequest } from 'firebase-functions/v2/https';
-
-import { App } from './main/app';
-import { FirestoreDatabase } from './main/databases';
-
-const database = FirestoreDatabase.create();
-const app = App.create(database);
-
-export const api = onRequest(app.express);
+import './main/server';
